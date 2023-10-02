53c0ee83abc77258ae40470b2b78baf0b16625f6
Marija Zlatkova <marijazlatkova44@yahoo.com>
2023-10-02 14:27:28 +0200

    domasna-5

---
+++ homework-5/src/main.jsx
@@ -0,0 +1,11 @@
+//! importiranje na virtuelniot dom
+import ReactDOM from "react-dom/client";
+
+//? importiranje na glavnata komponenta na aplikacijata
+import { App } from "./App.jsx";
+
+//* kreiranje na nova instanca od root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//! renderiranje na glavnata komponenta na aplikacijata
+root.render(<App />);
