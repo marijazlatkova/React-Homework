35ee56c07fd3e9e000e48edbe18ef5a90f36f316
Marija Zlatkova <marijazlatkova44@yahoo.com>
2023-10-12 12:47:22 +0200

    remove folder

--- homework-9/src/components/Home.jsx
+++
@@ -1,7 +0,0 @@
-export const Home = () => {
-  return (
-    <div id="home">
-      <h3>Gallery Exercise</h3>
-    </div>
-  );
-};
--- homework-9/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-import { App } from "./App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Router>
-    <App />
-  </Router>
-);
